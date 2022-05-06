269bff2ad5596d4f0a45b754a25ec2b627effd35
mahmoud <khairymahmoud795@gmail.com>
2022-05-07 01:21:25 +0200

    First Commit

---
+++ Simple Shell/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace simple_Shell
+{
+    public struct Token
+    {
+        public string command;
+        public string value;
+        public string sec_value;
+    }
+
+    class Program
+    {
+        public static string PATH_ON_PC = "C:\\Users\\Dell\\Desktop\\Simple Shell\\Simple Shell\\miniFat.txt";
+        public static Directory current;
+        public static string currentPath;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome, This Shell Is Developed By : Mohamed Sayed Osman, Mahmoud Khairy Abdelmoez, Mahmoud Ahmed Mahmoud:)");
+            Console.WriteLine("Section 3 - IS:)\n\n");
+            VirtualDisk.initialize(PATH_ON_PC);
+
+
+            currentPath = new string(current.dir_name);
+            currentPath = currentPath.Trim(new char[] { '\0', ' ' });
+
+
+            Parser parser = new Parser();
+
+
+
+            while (true)
+            {
+                var currentLocation = currentPath;
+                Console.Write(currentLocation + "\\>");
+                current.ReadDirectory();
+
+                string input = Console.ReadLine();
+                parser.parse_input(input);
+            }
+        }
+    }
+}
